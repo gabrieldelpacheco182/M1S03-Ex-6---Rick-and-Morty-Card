e2565d2b2e72a701a47f24cfdc9857eb25fce8f5
gabrieldelpacheco182 <gabriel_o_pacheco@estudante.sesisenai.org.br>
2023-03-11 20:51:54 -0300

    imagem colocada.

--- function.js
+++ function.js
@@ -1,10 +1,11 @@
 async function search() {
-    const response = await fetch('https://rickandmortyapi.com/api/character/215')
+    let personagem = document.getElementById('personagem').value;
+    const response = await fetch(`https://rickandmortyapi.com/api/character/${personagem}`)
     const data = await response.json()
     console.log(data)
     document.getElementById('name').innerHTML = data?.name
     document.getElementById('status').innerHTML = data?.status
     document.getElementById('specie').innerHTML = data?.species
-    document.getElementById('figura').innerHTML = data?.image
+    document.getElementById('figura').src = data?.image
 }
-search();
+
